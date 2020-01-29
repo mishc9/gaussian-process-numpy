26f8f678779bba2aa66b02e2c481888b3b438c38
Степанов Михаил <ml.stepanov@jet.su>
2020-01-29 15:46:04 +0300

    Renamed n_samples to n_test_samples

--- gp_train.py
+++ gp_train.py
@@ -5,13 +5,14 @@ from utils import kernel, get_train_data
 
 # Test data
 n = 50
-n_samples = 5
+n_test_samples = 10
 param = 0.25
 
 # Noiseless training data
 
 
-train_points = [-4, -3, -3.5, -2, -1, 1, 2, 3.5]
+# train_points = [-4, -3, -3.5, -2, -1, 1, 2, 3.5]
+train_points = [-4, -3.5, -2, -1, 1, 3.5]
 x_train, y_train = get_train_data(train_points, np.sin)
 x_test = np.linspace(-5, 5, n).reshape(-1, 1)
 
@@ -45,7 +46,7 @@ s2 = np.diag(K_ss) - np.sum(Lk ** 2, axis=0)
 stdv = np.sqrt(s2)
 # Draw samples from the posterior at our test points.
 L = np.linalg.cholesky(K_ss + 1e-6 * np.eye(n) - np.dot(Lk.T, Lk))
-f_post = mu.reshape(-1, 1) + np.dot(L, np.random.normal(size=(n, n_samples)))
+f_post = mu.reshape(-1, 1) + np.dot(L, np.random.normal(size=(n, n_test_samples)))
 
 plt.plot(x_train, y_train, 'bs', ms=8)
 plt.plot(x_test, f_post)
@@ -53,5 +54,5 @@ plt.plot(x_test, np.sin(x_test), c='k')
 plt.gca().fill_between(x_test.flat, mu - 2 * stdv, mu + 2 * stdv, color="#dddddd")
 plt.plot(x_test, mu, 'r--', lw=2)
 plt.axis([-5, 5, -3, 3])
-plt.title(f'{n_samples} samples from the GP posterior')
+plt.title(f'{n_test_samples} samples from the GP posterior')
 plt.show()
